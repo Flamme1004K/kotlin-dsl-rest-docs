df86a5039dac96f4ff3af6aaa560e309455d457a
flamme <flamme1004@gmail.com>
2022-09-11 21:29:49 +0900

    feat(RestDocs): body

---
+++ src/test/kotlin/com/restdocs/dsl/restDocs/BodyBuilder.kt
@@ -0,0 +1,35 @@
+package com.restdocs.dsl.restDocs
+
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
+import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
+import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.restdocs.payload.RequestFieldsSnippet
+import org.springframework.restdocs.payload.ResponseFieldsSnippet
+import org.springframework.restdocs.request.RequestDocumentation
+import org.springframework.restdocs.request.RequestParametersSnippet
+
+class BodyBuilder {
+    private val fields = mutableListOf<FieldDescriptor>()
+
+    infix fun String.means(
+        description: String
+    ): Field {
+        val fieldWithPath = PayloadDocumentation.fieldWithPath(this)
+        val field = Field(fieldWithPath).means(description)
+        fields.add(field.descriptor)
+        return field
+    }
+
+    fun build() = fields
+}
+
+fun responseBody(lists: BodyBuilder.() -> Unit): ResponseFieldsSnippet =
+    responseFields(
+        BodyBuilder().apply(lists).build()
+    )
+
+fun requestBody(lists: BodyBuilder.() -> Unit): RequestFieldsSnippet =
+    requestFields(
+        BodyBuilder().apply(lists).build()
+    )
