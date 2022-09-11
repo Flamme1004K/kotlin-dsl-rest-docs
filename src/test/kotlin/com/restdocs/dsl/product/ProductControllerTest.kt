cdf2ff09fbab7ee035b6e7ba968c705122ca7cbe
flamme <flamme1004@gmail.com>
2022-09-12 00:11:20 +0900

    feat(RestDocs): productController 변경

--- src/test/kotlin/com/restdocs/dsl/product/ProductControllerTest.kt
+++ src/test/kotlin/com/restdocs/dsl/product/ProductControllerTest.kt
@@ -1,58 +1,57 @@
 package com.restdocs.dsl.product
 
-import org.hamcrest.CoreMatchers.containsString
+import com.restdocs.dsl.restDocs.RestDocs
+import com.restdocs.dsl.restDocs.makeDocument
+import com.restdocs.dsl.restDocs.pathVariable
+import com.restdocs.dsl.restDocs.responseBody
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders
-import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
-import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
-import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
-import org.springframework.restdocs.request.RequestDocumentation.pathParameters
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(ProductController::class)
-@AutoConfigureRestDocs
-class ProductControllerTests {
-
-    @Autowired
-    private lateinit var mockMvc: MockMvc
+class ProductControllerTests : RestDocs() {
 
     @Test
-    fun `GET v1-product 200 ok`() { // 1
-
-        // When
-        val result = mockMvc.perform(get("/v1/products/2"))
-
-        // Then
-        result.andExpect(status().isOk)
-            .andExpect(MockMvcResultMatchers.content().string(containsString("Monitor")))
-    }
-
-    @Test
-    fun `GET v1-product 200 ok document`() { // 2
-        // When
-        val result = mockMvc.perform(RestDocumentationRequestBuilders.get("/v1/products/{code}", 2))
-
-        // Then
-        result.andExpect(status().isOk)
-            .andExpect(MockMvcResultMatchers.content().string(containsString("Monitor")))
-            .andDo(
-                document(
-                    "product/get-product-by-id",
-                    pathParameters(
-                        parameterWithName("code").description("Product Unique Identifier")
-                    ), responseFields(
-                        fieldWithPath("code").description("Product Unique Identifier"),
-                        fieldWithPath("name").description("Name of the product"),
-                        fieldWithPath("price").description("Product Price")
-                    )
-                )
+    fun `GET v1-product 200 ok`() {
+        given().perform(
+            RestDocumentationRequestBuilders.get(
+                "/v1/products/{code}",
+                2
             )
+        ).makeDocument(
+            "product/product-read",
+            MockMvcResultMatchers.status().isOk,
+            pathVariable {
+                "code" means "상품 번호"
+            },
+            responseBody {
+                "code" means "상품 번호" optional true constraints "a" format "0"
+                "name" means "상품 이름"
+                "price" means "상품 가격"
+            }
+        )
     }
+
+    // @Test
+    // fun `GET v1-product 200 ok document`() { // 2
+    //     // When
+    //     val result = mockMvc.perform(RestDocumentationRequestBuilders.get("/v1/products/{code}", 2))
+    //
+    //     // Then
+    //     result.andExpect(status().isOk)
+    //         .andExpect(MockMvcResultMatchers.content().string(containsString("Monitor")))
+    //         .andDo(
+    //             document(
+    //                 "product/get-product-by-id",
+    //                 pathParameters(
+    //                     parameterWithName("code").description("Product Unique Identifier")
+    //                 ), responseFields(
+    //                     fieldWithPath("code").description("Product Unique Identifier"),
+    //                     fieldWithPath("name").description("Name of the product"),
+    //                     fieldWithPath("price").description("Product Price")
+    //                 )
+    //             )
+    //         )
+    // }
 }
